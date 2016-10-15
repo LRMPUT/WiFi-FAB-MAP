b6bb637efbe26524755027b1944991aa2e584583
jachu51 <jan.m.wietrzykowski@gmail.com>
2016-10-15 10:25:33 +0200

    Ready for parameters tuning.

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,17 @@
 #include <chrono>
 #include <string>
 #include <memory>
+#include <chrono>
 
 #include <opencv2/opencv.hpp>
 
-#include "../include/openfabmap.hpp"
+#include "openfabmap.hpp"
+#include "Evaluation.hpp"
 
 using namespace std;
 using namespace cv;
 
 static constexpr int descSize = 520;
-static constexpr float matchProbThresh = 0.0;
 static constexpr float eps = 1e-6;
 
 template<class T>
@@ -186,17 +187,23 @@ void preprocessDescs(cv::Mat& descs){
 	static constexpr int minVal = -110;
 	static constexpr int maxVal = 0;
 	static constexpr int stepVal = 5;
+	static constexpr int notFoundVal = 100;
 
 	descs -= Scalar(minVal);
-	Mat notFoundMask = (descs == 100 - minVal);
-	descs.setTo(Scalar(0), notFoundMask);
+//	Mat notFoundMask = (descs == notFoundVal - minVal);
+//	descs.setTo(Scalar(0), notFoundMask);
 	int binsPerDesc = ((maxVal - minVal)/stepVal);
 	Mat extDescs(descs.rows, binsPerDesc * descs.cols, CV_32FC1, Scalar(0));
 	for(int d = 0; d < descs.rows; ++d){
 		for(int v = 0; v < descs.cols; ++v){
 //			cout << "v = " << v << endl;
-			if(fabs(descs.at<float>(d, v)) > eps){
+			if(fabs(descs.at<float>(d, v) - (notFoundVal - minVal)) > eps){
 				float val = descs.at<float>(d, v);
+//				std::default_random_engine gen;
+//				std::uniform_int_distribution<int> dist(-30, 30);
+//				val += dist(gen);
+//				val = max(val, 0.0f);
+//				val = min(val, (float)(maxVal - minVal));
 				int bin = val/stepVal;
 				for(int b = 0; b <= bin; ++b){
 					extDescs.at<float>(d, v * binsPerDesc + b) = 1.0;
@@ -242,7 +249,7 @@ void assignClusters(const std::vector<cv::Point2f>& srcPoses,
 	dstClust = vector<int>(dstPoses.size(), -1);
 	cout << "Analyzing matches" << endl;
 	for(int i = 0; i < matches.size(); ++i){
-		cout << "matches[" << i << "].distance = " << matches[i].distance << endl;
+//		cout << "matches[" << i << "].distance = " << matches[i].distance << endl;
 		if(matches[i].distance <= distEps){
 			if(matches[i].trainIdx >= 0 && matches[i].trainIdx < srcClust.size()){
 				dstClust[i] = srcClust[matches[i].trainIdx];
@@ -251,80 +258,6 @@ void assignClusters(const std::vector<cv::Point2f>& srcPoses,
 	}
 }
 
-void evaluate(cv::Mat trainDescs,
-				std::vector<int> trainBuildings,
-				std::vector<int> trainFloors,
-				std::vector<cv::Point2f> trainPoses,
-				cv::Mat testDescs,
-				std::vector<int> testBuildings,
-				std::vector<int> testFloors,
-				std::vector<cv::Point2f> testPoses,
-				int& correct,
-				int& incorrect,
-				int& unrec,
-				float PzGe,
-				float PzGne,
-				cv::Mat clTree)
-{
-	shared_ptr<of2::FabMap> fabmap(new of2::FabMap2(clTree,
-													PzGe,
-													PzGne,
-													of2::FabMap::SAMPLED | of2::FabMap::CHOW_LIU));
-
-//			cout << "Adding train data" << endl;
-	fabmap->addTraining(trainDescs);
-
-	cout << "Adding places database" << endl;
-	fabmap->add(trainDescs);
-
-	cout << "Matching" << endl;
-	correct = 0;
-	incorrect = 0;
-	unrec = 0;
-	int all = 0;
-	float distErr = 0.0;
-	for(int d = 0; d < testDescs.rows; ++d){
-		std::vector<of2::IMatch> matches;
-		fabmap->compare(testDescs.row(d), matches, false);
-		double bestMatchProb = 0.0;
-		int bestMatchIdx = -1;
-//		std::cout << "matches.size() = " << matches.size() << std::endl;
-		for(std::vector<of2::IMatch>::iterator it = matches.begin(); it != matches.end(); ++it){
-//			cout << "it->match = " << it->match << endl;
-			if(it->match > matchProbThresh){
-				if(bestMatchProb < it->match){
-					bestMatchProb = it->match;
-					bestMatchIdx = it->imgIdx;
-				}
-			}
-		}
-//		std::cout << "Matched with " << bestMatchIdx << ", prob = " << bestMatchProb << std::endl;
-		if(bestMatchIdx >= 0){
-//						cout << "trainClusters[bestMatchIdx] = " << trainClusters[bestMatchIdx] << endl;
-//						cout << "testClusters[d] = " << testClusters[d] << endl;
-			if(trainBuildings[bestMatchIdx] == testBuildings[d] &&
-				trainFloors[bestMatchIdx] == testFloors[d])
-			{
-//			if(trainClusters[bestMatchIdx] == testClusters[d])
-//			{
-				++correct;
-
-				distErr += norm(trainPoses[bestMatchIdx] - testPoses[d]);
-			}
-			else{
-				++incorrect;
-			}
-		}
-		else{
-			++unrec;
-		}
-	}
-	cout << "correct = " << correct << endl;
-	cout << "incorrect = " << incorrect << endl;
-	cout << "unrec = " << unrec << endl;
-	cout << "success rate = " << (double)correct / (correct + incorrect + unrec) << endl;
-	cout << "mean distError = " << distErr/correct << endl;
-}
 
 
 int main(int argc, char * argv[]){
@@ -377,12 +310,27 @@ int main(int argc, char * argv[]){
 //		cout << trainDescs.row(d) << endl;
 //	}
 
+	cv::Mat trainEnvDescs;
+	std::vector<cv::Point2f> trainEnvPoses;
+	std::vector<int> trainEnvFloors;
+	std::vector<int> trainEnvBuildings;
+	std::vector<int> trainEnvClusters;
+
+	loadDataFileClust("../res/trainingEnvDataClust.csv",
+						trainEnvDescs,
+						trainEnvPoses,
+						trainEnvFloors,
+						trainEnvBuildings,
+						trainEnvClusters);
+
+	preprocessDescs(trainEnvDescs);
 
 	cv::Mat clTree;
 	if((int)fsSettings["ChowLiuOptions"]["Train"] > 0){
 		cout << "Learning Chow-Liu Tree" << endl;
+
 		of2::ChowLiuTree tree;
-		tree.add(trainDescs);
+		tree.add(trainEnvDescs);
 		clTree = tree.make((double)fsSettings["ChowLiuOptions"]["LowerInfoBound"]);
 
 		//save the resulting tree
@@ -402,126 +350,237 @@ int main(int argc, char * argv[]){
 		fsChowLiu.release();
 	}
 
-	// random permutation for splitting training and validation set
-	std::default_random_engine gen;
-	std::vector<int> perm;
-	for(int i = 0; i < trainDescs.rows; ++i){
-		perm.push_back(i);
-	}
-	for(int i = 1; i < perm.size(); ++i){
-		std::uniform_int_distribution<int> dist(0, i);
-		int swapIdx = dist(gen);
-		swap(perm[i], perm[swapIdx]);
-	}
+	static constexpr bool tuneParams = true;
+	if(tuneParams){
+		// random permutation for splitting training and validation set
+		std::default_random_engine gen;
+		std::vector<int> perm;
+		for(int i = 0; i < trainDescs.rows; ++i){
+			perm.push_back(i);
+		}
+		for(int i = 1; i < perm.size(); ++i){
+			std::uniform_int_distribution<int> dist(0, i);
+			int swapIdx = dist(gen);
+			swap(perm[i], perm[swapIdx]);
+		}
+//		cout << perm << endl;
 
-	static constexpr float valPart = 0.2;
-	int trainStart = perm.size() * valPart;
+		static constexpr float valPart = 0.33;
+		int trainStart = perm.size() * valPart;
 
-	cv::Mat testSelDescs(0, trainDescs.cols, CV_32FC1);
-	std::vector<int> testSelBuildings;
-	std::vector<int> testSelFloors;
-	std::vector<cv::Point2f> testSelPoses;
-	for(int i = 0; i < trainStart; ++i){
-		testSelDescs.push_back(trainDescs.row(perm[i]));
-		testSelBuildings.push_back(trainBuildings[perm[i]]);
-		testSelFloors.push_back(trainFloors[perm[i]]);
-		testSelPoses.push_back(trainPoses[perm[i]]);
-	}
+		cv::Mat testSelDescs(0, trainDescs.cols, CV_32FC1);
+		std::vector<int> testSelBuildings;
+		std::vector<int> testSelFloors;
+		std::vector<cv::Point2f> testSelPoses;
+		for(int i = 0; i < trainStart; ++i){
+			testSelDescs.push_back(trainDescs.row(perm[i]));
+			testSelBuildings.push_back(trainBuildings[perm[i]]);
+			testSelFloors.push_back(trainFloors[perm[i]]);
+			testSelPoses.push_back(trainPoses[perm[i]]);
+		}
 
-	cv::Mat trainSelDescs(0, trainDescs.cols, CV_32FC1);
-	std::vector<int> trainSelBuildings;
-	std::vector<int> trainSelFloors;
-	std::vector<cv::Point2f> trainSelPoses;
-	for(int i = trainStart; i < perm.size(); ++i){
-		trainSelDescs.push_back(trainDescs.row(perm[i]));
-		trainSelBuildings.push_back(trainBuildings[perm[i]]);
-		trainSelFloors.push_back(trainFloors[perm[i]]);
-		trainSelPoses.push_back(trainPoses[perm[i]]);
-	}
+//		// additionally noise data
+//		for(int iter = 0; iter < 20; ++iter){
+//			for(int d = 0; d < testSelDescs.rows; ++d){
+//				bool wasOne = false;
+//				for(int f = 0; f < testSelDescs.cols; f += 22){
+//					int pos = 0;
+//					while(pos < 22 && fabs(testSelDescs.at<float>(d, f) - 1.0) < eps){
+//						++pos;
+//					}
+//					std::uniform_int_distribution<int> dist(0, 2);
+//					int op = dist(gen);
+//					// erase last 1
+//					if(op == 0 && pos > 0){
+//						testSelDescs.at<float>(d, f + pos - 1) = 0.0;
+//					}
+//					// add new 1
+//					else if(op == 1 && pos < 22){
+//						testSelDescs.at<float>(d, f + pos) = 1.0;
+//					}
+//					// do nothing
+//					else{
+//
+//					}
+//				}
+//			}
+//		}
 
-	cout << "trainSelDescs.rows = " << trainSelDescs.rows << endl;
-	cout << "testSelDescs.rows = " << testSelDescs.rows << endl;
+		cv::Mat trainSelDescs(0, trainDescs.cols, CV_32FC1);
+		std::vector<int> trainSelBuildings;
+		std::vector<int> trainSelFloors;
+		std::vector<cv::Point2f> trainSelPoses;
+		for(int i = trainStart; i < perm.size(); ++i){
+			trainSelDescs.push_back(trainDescs.row(perm[i]));
+			trainSelBuildings.push_back(trainBuildings[perm[i]]);
+			trainSelFloors.push_back(trainFloors[perm[i]]);
+			trainSelPoses.push_back(trainPoses[perm[i]]);
+		}
 
-	static constexpr float pzgeStart = -0.01;
-	static constexpr float pzgeStep = -0.05;
-	static constexpr float pzgeStop = -10;
-	static constexpr float pzgneStart = -0.01;
-	static constexpr float pzgneStep = -0.05;
-	static constexpr float pzgneStop = -10;
-//	cout << (double)fsSettings["openFabMapOptions"]["PzGe"] << endl;
-//	cout << (double)fsSettings["openFabMapOptions"]["PzGne"] << endl;
+		cout << "trainSelDescs.rows = " << trainSelDescs.rows << endl;
+		cout << "testSelDescs.rows = " << testSelDescs.rows << endl;
 
-	float bestScore = 0.0;
-	float bestPzge, bestPzgne;
-	ofstream resultsFile("validation.log");
-	if(!resultsFile.good()){
-		cout << "Error - couldn't open log file" << endl;
-	}
-	for(float curPzge = pzgeStart; curPzge >= pzgeStop; curPzge += pzgeStep){
-		for(float curPzgne = pzgneStart; curPzgne >= pzgneStop; curPzgne += pzgneStep){
-			int correct;
-			int incorrect;
-			int unrec;
+//		static constexpr float pzgeStart = -0.94148;
+		static constexpr float pzgeStart = -0.01;
+		static constexpr float pzgeStep = -0.05;
+		static constexpr float pzgeStop = -4;
+		static constexpr float pzgneStart = -4;
+		static constexpr float pzgneStep = -0.05;
+		static constexpr float pzgneStop = -10;
 
-			cout << "curPzge = " << curPzge << " (" << exp(curPzge) << ")" << endl;
-			cout << "curPzgne = " << curPzgne << " (" << exp(curPzgne) << ")" << endl;
+		static constexpr int maxThreads = 8;
 
-			evaluate(trainSelDescs,
-					trainSelBuildings,
-					trainSelFloors,
-					trainSelPoses,
-					testSelDescs,
-					testSelBuildings,
-					testSelFloors,
-					testSelPoses,
-					correct,
-					incorrect,
-					unrec,
-					exp(curPzge),
-					exp(curPzgne),
-					clTree);
+		float bestScore = 0.0;
+		float bestPzge, bestPzgne;
+		ofstream resultsFile("validation.log");
+		if(!resultsFile.good()){
+			cout << "Error - couldn't open log file" << endl;
+		}
 
-			float curScore = (float)correct/(correct + incorrect + unrec);
-			if(curScore > bestScore){
-				bestScore = curScore;
-				bestPzge = curPzge;
-				bestPzgne = curPzgne;
+		std::vector<std::vector<float>> logPzges;
+		std::vector<std::vector<float>> logPzgnes;
+		for(float curPzge = pzgeStart; curPzge >= pzgeStop; curPzge += pzgeStep){
+			logPzges.emplace_back();
+			logPzgnes.emplace_back();
+			for(float curPzgne = pzgneStart; curPzgne >= pzgneStop; curPzgne += pzgneStep){
+				logPzges.back().emplace_back(curPzge);
+				logPzgnes.back().emplace_back(curPzgne);
 			}
-			resultsFile << curPzge << " " << curPzgne << " " << curScore << endl;
+		}
+		std::vector<std::vector<std::shared_ptr<EvaluationThread>>> threads;
+		std::vector<std::vector<vector<int>>> matchedTestIdxs;
+		std::vector<std::vector<int>> correct;
+		std::vector<std::vector<int>> incorrect;
+		std::vector<std::vector<int>> unrec;
+		std::vector<std::vector<float>> results;
+		for(int i = 0; i < logPzges.size(); ++i){
+			matchedTestIdxs.emplace_back(logPzges[i].size());
+			threads.emplace_back(logPzges[i].size());
+			correct.emplace_back(logPzges[i].size());
+			incorrect.emplace_back(logPzges[i].size());
+			unrec.emplace_back(logPzges[i].size());
+			results.emplace_back(logPzges[i].size());
+		}
+		int threadCnt = 0;
+		for(int i = 0; i < logPzges.size(); ++i){
+			for(int j = 0; j < logPzges[i].size(); ++j){
+
+				while(threadCnt >= maxThreads){
+					endThreadIfAny(threads,
+									matchedTestIdxs,
+									correct,
+									incorrect,
+									unrec,
+									results,
+									threadCnt,
+									bestScore,
+									bestPzge,
+									bestPzgne,
+									resultsFile);
+					std::this_thread::sleep_for(chrono::milliseconds(50));
+				}
+
+				cout << "curPzge = " << logPzges[i][j] << " (" << exp(logPzges[i][j]) << ")" << endl;
+				cout << "curPzgne = " << logPzgnes[i][j] << " (" << exp(logPzgnes[i][j]) << ")" << endl;
+
+				correct.back().emplace_back();
+				incorrect.back().emplace_back();
+				unrec.back().emplace_back();
+				results.back().emplace_back();
+				threads[i][j] = shared_ptr<EvaluationThread>(new EvaluationThread(trainEnvDescs,
+																trainSelDescs,
+																trainSelBuildings,
+																trainSelFloors,
+																trainSelPoses,
+																testSelDescs,
+																testSelBuildings,
+																testSelFloors,
+																testSelPoses,
+																matchedTestIdxs[i][j],
+																correct[i][j],
+																incorrect[i][j],
+																unrec[i][j],
+																exp(logPzges[i][j]),
+																exp(logPzgnes[i][j]),
+																clTree));
+				++threadCnt;
+			}
+		}
+
+		resultsFile.close();
+		cout << "bestScore = " << bestScore << endl;
+		cout << "bestPzge = " << bestPzge << endl;
+		cout << "bestPzgne = " << bestPzgne << endl;
+	}
+	else{
+		cout << "Recognizing places" << endl;
+		cv::Mat testDescs;
+		std::vector<cv::Point2f> testPoses;
+		std::vector<int> testFloors;
+		std::vector<int> testBuildings;
+		std::vector<int> testClusters;
+
+		loadDataFile("../res/validationData.csv",
+					testDescs,
+					testPoses,
+					testFloors,
+					testBuildings);
+
+//		for(int d = 0; d < min(10, testDescs.rows); ++d){
+//			cout << testDescs.row(d) << endl;
+//		}
+
+		preprocessDescs(testDescs);
+
+//		for(int d = 0; d < min(10, trainDescs.rows); ++d){
+//			cout << trainDescs.row(d) << endl;
+//		}
+//		for(int d = 0; d < min(10, testDescs.rows); ++d){
+//			cout << testDescs.row(d) << endl;
+//		}
+
+		cout << "Assigning train clusters" << endl;
+
+		assignClusters(trainPoses,
+						trainFloors,
+						trainClusters,
+						testPoses,
+						testFloors,
+						testClusters,
+						6.0);
+		cout << (double)fsSettings["openFabMapOptions"]["PzGe"] << endl;
+		cout << (double)fsSettings["openFabMapOptions"]["PzGne"] << endl;
+
+		vector<int> matchedTestIdxs;
+		int correct;
+		int incorrect;
+		int unrec;
+
+		shared_ptr<EvaluationThread> evThread(new EvaluationThread(trainEnvDescs,
+																	trainDescs,
+																	trainBuildings,
+																	trainFloors,
+																	trainPoses,
+																	testDescs,
+																	testBuildings,
+																	testFloors,
+																	testPoses,
+																	matchedTestIdxs,
+																	correct,
+																	incorrect,
+																	unrec,
+																	(double)fsSettings["openFabMapOptions"]["PzGe"],
+																	(double)fsSettings["openFabMapOptions"]["PzGne"],
+																	clTree));
+
+		while(!evThread->hasEnded()){
+			std::this_thread::sleep_for(chrono::milliseconds(50));
+		}
+		evThread.reset();
+
+		ofstream matchedIdxsFile("matches.log");
+		for(int i = 0; i < matchedTestIdxs.size(); ++i){
+			matchedIdxsFile << matchedTestIdxs[i] << endl;
 		}
 	}
-
-	resultsFile.close();
-	cout << "bestScore = " << bestScore << endl;
-	cout << "bestPzge = " << bestPzge << endl;
-	cout << "bestPzgne = " << bestPzgne << endl;
-
-//	cout << "Recognizing places" << endl;
-//	cv::Mat testDescs;
-//	std::vector<cv::Point2f> testPoses;
-//	std::vector<int> testFloors;
-//	std::vector<int> testBuildings;
-//	std::vector<int> testClusters;
-//
-//	loadDataFile("../res/validationData.csv",
-//				testDescs,
-//				testPoses,
-//				testFloors,
-//				testBuildings);
-//
-//	preprocessDescs(testDescs);
-//
-////	for(int d = 0; d < min(10, testDescs.rows); ++d){
-////		cout << testDescs.row(d) << endl;
-////	}
-//
-//	cout << "Assigning train clusters" << endl;
-//
-//	assignClusters(trainPoses,
-//					trainFloors,
-//					trainClusters,
-//					testPoses,
-//					testFloors,
-//					testClusters,
-//					6.0);
 }
